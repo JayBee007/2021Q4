f3f4363e41f3138a2cffb7de07e397efbeb88c29
Javed <>
2021-11-03 23:24:47 +0300

    feat: add custom error handling to news cli

--- rust/news_cli/newsapi/src/lib.rs
+++ rust/news_cli/newsapi/src/lib.rs
@@ -1,7 +1,16 @@
-use std::error::Error;
+use thiserror::Error;
 use ureq::get;
 use serde::{Deserialize};
 
+#[derive(Error, Debug)]
+pub enum NewsApiError {
+    #[error("Fetching articles failed")]
+    RequestFailed(ureq::Error),
+    #[error("Couldnt convert response to string")]
+    FailedResToString(std::io::Error),
+    #[error("Parsing article failed")]
+    ParseArticleFailed(serde_json::Error),
+}
 
 #[derive(Debug, Deserialize)]
 pub struct Article {
@@ -14,10 +23,10 @@ pub struct Articles{
     pub articles: Vec<Article>
 }
 
-pub fn get_articles(url: String) -> Result<Articles, Box<dyn Error>> {
-    let response = get(&url).call()?.into_string()?;
+pub fn get_articles(url: String) -> Result<Articles, NewsApiError> {
+    let response = get(&url).call().map_err(|err| NewsApiError::RequestFailed(err))?.into_string().map_err(|err| NewsApiError::FailedResToString(err))?;
 
-    let articles:Articles = serde_json::from_str(&response)?;
+    let articles:Articles = serde_json::from_str(&response).map_err(|err| NewsApiError::ParseArticleFailed(err))?;
 
     Ok(articles)
 }
