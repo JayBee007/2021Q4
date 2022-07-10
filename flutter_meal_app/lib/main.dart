768906a727a82fe3b2ccbe812c4e389d2d0755a9
Javed <jaybee.codes@gmail.com>
2022-07-10 21:15:36 +0300

    feat (flutter-meal-app): add custom font and color palette

--- flutter_meal_app/lib/main.dart
+++ flutter_meal_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './config/palette.dart';
 import './screens/categories_screen.dart';
 
 void main() => runApp(const MealApp());
@@ -7,14 +8,28 @@ void main() => runApp(const MealApp());
 class MealApp extends StatelessWidget {
   const MealApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Meals',
       theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),home: CategoriesScreen(key: key),);
+        primarySwatch: Palette.palette,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Palette.paletteAccent, // Your accent color
+        ),
+        canvasColor:  const Color.fromARGB(222, 255, 252, 252),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          bodyText2: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          headline6: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold
+          )
+        )
+      ),
+      home: CategoriesScreen(key: key),
+    );
   }
 }
-
